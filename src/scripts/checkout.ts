e6566a694d501c3d2df7b3313c49afa046192761
nedaras <nedas.pranskunas@gmail.com>
2024-08-10 01:33:39 +0300

    We're not downloading whole internet on page load.

--- src/scripts/checkout.ts
+++ src/scripts/checkout.ts
@@ -1,5 +1,3 @@
-import {loadStripe} from '@stripe/stripe-js'
-
 document.body.addEventListener('htmx:beforeSwap', (e) => {
   // make it on htmx-request or smth i need to extend the indicator
   if (e.detail.isError) return
@@ -10,6 +8,8 @@ document.body.addEventListener('htmx:beforeSwap', (e) => {
 })
 
 async function showStripe(clientSecret: string) {
+  const loadStripe = (await import('@stripe/stripe-js')).loadStripe
+
   const stripe = await loadStripe('pk_test_51Plzla09srk6l0GXkHcOPFrRM2z2zUMhs8VraClycfkq9oUBeCzLKehc6RfTAbaIQQ6fPKDLAAeNBNeFBizV0vlt00cDaxIkDF')
   const checkout = await stripe?.initEmbeddedCheckout({clientSecret})
 
